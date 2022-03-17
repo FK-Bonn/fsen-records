5b6d93d0a94e7762664c23d8198596035469ec71
HSZemi <hszemi@users.noreply.github.com>
2022-03-17 18:19:09 +0100

    Display additional semesters
    Add 2018-SoSe until 2019-WiSe

--- src/sections/StudentBody.svelte
+++ src/sections/StudentBody.svelte
@@ -34,6 +34,10 @@
         <SemesterSection semester={Interval.fromStrings('2021-04-01', '2021-09-30')} {studentBody}/>
         <SemesterSection semester={Interval.fromStrings('2020-10-01', '2021-03-31')} {studentBody}/>
         <SemesterSection semester={Interval.fromStrings('2020-04-01', '2020-09-30')} {studentBody}/>
+        <SemesterSection semester={Interval.fromStrings('2019-10-01', '2020-03-31')} {studentBody}/>
+        <SemesterSection semester={Interval.fromStrings('2019-04-01', '2019-09-30')} {studentBody}/>
+        <SemesterSection semester={Interval.fromStrings('2018-10-01', '2019-03-31')} {studentBody}/>
+        <SemesterSection semester={Interval.fromStrings('2018-04-01', '2018-09-30')} {studentBody}/>
 
         <DocumentsSection {studentBody}/>
 
