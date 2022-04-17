5459afa9031736378748d6fa6fa9f66d2b1249fd
HSZemi <hszemi@users.noreply.github.com>
2022-04-17 19:18:05 +0200

    Persist options in localStorage

--- src/stores.ts
+++ src/stores.ts
@@ -1,7 +1,43 @@
-import { writable } from 'svelte/store';
+import {writable} from 'svelte/store';
 
-export const showOnlyWhoCurrentlyCanBePaid = writable(false);
-export const showOnlySemestersWithPayoutRequests = writable(false);
-export const showOnlySemestersWithStar = writable(false);
-export const compactMode = writable(false);
-export const showFilenames = writable(true);
+const lsGetBool = (key: string, default_value: boolean = false): boolean => {
+    try {
+        const value = localStorage.getItem(key);
+        if (value === null) {
+            return default_value;
+        }
+        return value === 'true';
+    } catch (e) {
+        return default_value;
+    }
+}
+
+const lsSetBool = (key: string, value: boolean) => {
+    try {
+        localStorage.setItem(key, value ? 'true' : 'false');
+    } catch (e) {
+        //pass
+    }
+}
+
+export const showOnlyWhoCurrentlyCanBePaid = writable(lsGetBool('showOnlyWhoCurrentlyCanBePaid'));
+export const showOnlySemestersWithPayoutRequests = writable(lsGetBool('showOnlySemestersWithPayoutRequests'));
+export const showOnlySemestersWithStar = writable(lsGetBool('showOnlySemestersWithStar'));
+export const compactMode = writable(lsGetBool('compactMode'));
+export const showFilenames = writable(lsGetBool('showFilenames', true));
+
+showOnlyWhoCurrentlyCanBePaid.subscribe(value => {
+    lsSetBool("showOnlyWhoCurrentlyCanBePaid", value);
+});
+showOnlySemestersWithPayoutRequests.subscribe(value => {
+    lsSetBool("showOnlySemestersWithPayoutRequests", value);
+});
+showOnlySemestersWithStar.subscribe(value => {
+    lsSetBool("showOnlySemestersWithStar", value);
+});
+compactMode.subscribe(value => {
+    lsSetBool("compactMode", value);
+});
+showFilenames.subscribe(value => {
+    lsSetBool("showFilenames", value);
+});
