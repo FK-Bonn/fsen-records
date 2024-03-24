44c6cd0eea754e0fe619648aa8a954b511bfc45d
HSZemi <hszemi@users.noreply.github.com>
2024-03-24 15:25:16 +0100

    Add Documents Sections

---
+++ src/components/document/DocumentsList.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+
+import type {IStudentBody} from "@/interfaces";
+import SingleDocument from "@/components/document/SingleDocument.vue";
+
+defineProps<{
+  studentBody: IStudentBody,
+}>()
+</script>
+
+<template>
+  <div class="card-content">
+    <div class="content">
+      <h3 class="heading is-3">Haushaltspläne</h3>
+      <template v-if="studentBody.budgets.length > 0">
+        <ul>
+          <li v-for="budget in studentBody.budgets" :key="budget.filename">
+            <SingleDocument :document="budget" :studentBody="studentBody"/>
+          </li>
+        </ul>
+      </template>
+      <template v-else>
+        Keine vorhanden.
+      </template>
+      <h3 class="heading is-3">Haushaltsrechnungen</h3>
+      <template v-if="studentBody.balances.length > 0">
+        <ul>
+          <li v-for="balance in studentBody.balances" :key="balance.filename">
+            <SingleDocument :document="balance" :studentBody="studentBody"/>
+          </li>
+        </ul>
+      </template>
+      <template v-else>
+        Keine vorhanden.
+      </template>
+      <h3 class="heading is-3">Kassenprüfungen</h3>
+      <template v-if="studentBody.cashAudits.length > 0">
+        <ul>
+          <li v-for="cashAudit in studentBody.cashAudits" :key="cashAudit.filename">
+            <SingleDocument :document="cashAudit" :studentBody="studentBody"/>
+          </li>
+        </ul>
+      </template>
+      <template v-else>
+        Keine vorhanden.
+      </template>
+      <h3 class="heading is-3">Protokolle</h3>
+      <template v-if="studentBody.proceedings.length > 0">
+        <ul>
+          <li v-for="proceeding in studentBody.proceedings" :key="proceeding.filename">
+            <SingleDocument :document="proceeding" :studentBody="studentBody"/>
+          </li>
+        </ul>
+      </template>
+      <template v-else>
+        Keine vorhanden.
+      </template>
+      <h3 class="heading is-3">Wahlergebnisse</h3>
+      <template v-if="studentBody.electionResults.length > 0">
+        <ul>
+          <li v-for="electionResult in studentBody.electionResults" :key="electionResult.filename">
+            <SingleDocument :document="electionResult" :studentBody="studentBody"/>
+          </li>
+        </ul>
+      </template>
+      <template v-else>
+        Keine vorhanden.
+      </template>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+li {
+  list-style: none;
+}
+</style>
--- src/components/studentbody/DocumentsSection.vue
+++ src/components/studentbody/DocumentsSection.vue
@@ -1,11 +1,36 @@
 <script setup lang="ts">
 
+import AngleIndicator from "@/components/icons/AngleIndicator.vue";
+import {ref} from "vue";
+import type {IStudentBody} from "@/interfaces";
+import DocumentsList from "@/components/document/DocumentsList.vue";
+
+defineProps<{
+  studentBody: IStudentBody,
+}>()
+
+const opened = ref(false);
+const toggle = () => {
+  opened.value = !opened.value;
+}
 </script>
 
 <template>
-
+  <div class="card">
+    <header class="card-header" @click="toggle">
+      <p class="card-header-title">
+        Dokumente
+      </p>
+      <button class="card-header-icon" aria-label="more options">
+        <AngleIndicator :opened="opened"/>
+      </button>
+    </header>
+    <DocumentsList v-if="opened" :studentBody="studentBody"/>
+  </div>
 </template>
 
 <style scoped>
-
+.card-header {
+  cursor: pointer;
+}
 </style>
