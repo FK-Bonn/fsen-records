de3e5c3907b43b5daf763aedfeeb9615dd3d9ade
HSZemi <hszemi@users.noreply.github.com>
2026-02-03 23:26:03 +0100

    Update and rename request category options

--- src/components/payoutrequest/RequestCategoryOptions.vue
+++ src/components/payoutrequest/RequestCategoryOptions.vue
@@ -7,14 +7,15 @@
 <option value='[b] Inhaltliche Veranstaltungen'>[b] Inhaltliche Veranstaltungen</option>
 <option value='[c] Erstsemesterfahrt'>[c] Erstsemesterfahrt</option>
 <option value='[d] Klausurfahrten'>[d] Klausurfahrten</option>
-<option value='[e] Teilnahme BuFaTa'>[e] Teilnahme BuFaTa</option>
+<option value='[e] Teilnahme Fachschaftentagung'>[e] Teilnahme Fachschaftentagung</option>
 <option value='[f] Bildungsfahrt'>[f] Bildungsfahrt</option>
 <option value='[g] Tagesexkursion'>[g] Tagesexkursion</option>
-<option value='[h] Computer und Zubehör'>[h] Computer und Zubehör</option>
-<option value='[i] Ausrichtung BuFaTa'>[i] Ausrichtung BuFaTa</option>
+<option value='[h] Sachmittel'>[h] Sachmittel</option>
+<option value='[i] Ausrichtung Fachschaftentagung'>[i] Ausrichtung Fachschaftentagung</option>
 <option value='[j] Fachschaftskleidung'>[j] Fachschaftskleidung</option>
 <option value='[k] Fachschaftsneugründung'>[k] Fachschaftsneugründung</option>
 <option value='[l] Schulungen'>[l] Schulungen</option>
+<option value='[m] Wiederaufbauunterstützung'>[m] Wiederaufbauunterstützung</option>
 <option value='Außerhalb KritKat'>Außerhalb KritKat</option>
 </template>
 
